17942719c1e7987dd0eb0d1abc09d7a4960bd241
Noiroup <noiroup@gmail.com>
2024-09-19 11:54:42 -0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,5 +3,5 @@ const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
 
 burger.addEventListener("click", () => {
-  ul.classList.toggle("show-ul");
+  ul.classList.toggle("show");
 });
