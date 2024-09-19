a30cd7c1d466cff918ec9b5897e5b607f53fd898
Noiroup <noiroup@gmail.com>
2024-09-19 09:50:04 -0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,5 +2,5 @@ const menue = document.querySelector(".menu");
 const ul = document.querySelector(".links");
 
 menue.addEventListener("click", function() {
-   ul.classList.toggle("nav ul"); 
+   nav.classList.toggle("show-links"); 
 })
