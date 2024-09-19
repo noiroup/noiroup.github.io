e9c65dfb4ecab8c6c1c95e2aa52eaf606bae889d
Noiroup <noiroup@gmail.com>
2024-09-19 08:44:29 -0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ scrollUp.addEventListener("click", () => {
 });
 
 
-const burger = document.querySelector("#burger-menu");
+const menu = document.querySelector("#menu");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
 
